e0a1d067d59d9a4ef1c167a40797e81b6027df03
AlexTenFit <alexwww9@gmail.com>
2025-07-30 19:15:37 +0300

    added player and movement

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,8 +22,14 @@ def main():
 
         screen.fill("black")
 
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
+
+        # 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
